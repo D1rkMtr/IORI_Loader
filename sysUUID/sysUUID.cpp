90db2d6bbaf607c77e1a1c86598bef0b838314b5
D1rkMtr <110354855+D1rkMtr@users.noreply.github.com>
2022-10-29 05:35:46 +0100

    Update sysUUID.cpp

--- sysUUID/sysUUID.cpp
+++ sysUUID/sysUUID.cpp
@@ -198,6 +198,138 @@ WORD Unh00ksyscallNum(LPVOID addr) {
         }
 
     }
+
+    if (*((PBYTE)addr + 3) == 0xe9) {
+
+        for (WORD idx = 1; idx <= 500; idx++) {
+            if (*((PBYTE)addr + idx * DOWN) == 0x4c
+                && *((PBYTE)addr + 1 + idx * DOWN) == 0x8b
+                && *((PBYTE)addr + 2 + idx * DOWN) == 0xd1
+                && *((PBYTE)addr + 3 + idx * DOWN) == 0xb8
+                && *((PBYTE)addr + 6 + idx * DOWN) == 0x00
+                && *((PBYTE)addr + 7 + idx * DOWN) == 0x00) {
+                BYTE high = *((PBYTE)addr + 5 + idx * DOWN);
+                BYTE low = *((PBYTE)addr + 4 + idx * DOWN);
+                syscall = (high << 8) | low - idx;
+
+                return syscall;
+            }
+            if (*((PBYTE)addr + idx * UP) == 0x4c
+                && *((PBYTE)addr + 1 + idx * UP) == 0x8b
+                && *((PBYTE)addr + 2 + idx * UP) == 0xd1
+                && *((PBYTE)addr + 3 + idx * UP) == 0xb8
+                && *((PBYTE)addr + 6 + idx * UP) == 0x00
+                && *((PBYTE)addr + 7 + idx * UP) == 0x00) {
+                BYTE high = *((PBYTE)addr + 5 + idx * UP);
+                BYTE low = *((PBYTE)addr + 4 + idx * UP);
+                syscall = (high << 8) | low + idx;
+
+                return syscall;
+
+            }
+
+        }
+
+    }
+
+    if (*((PBYTE)addr + 8) == 0xe9) {
+
+        for (WORD idx = 1; idx <= 500; idx++) {
+            if (*((PBYTE)addr + idx * DOWN) == 0x4c
+                && *((PBYTE)addr + 1 + idx * DOWN) == 0x8b
+                && *((PBYTE)addr + 2 + idx * DOWN) == 0xd1
+                && *((PBYTE)addr + 3 + idx * DOWN) == 0xb8
+                && *((PBYTE)addr + 6 + idx * DOWN) == 0x00
+                && *((PBYTE)addr + 7 + idx * DOWN) == 0x00) {
+                BYTE high = *((PBYTE)addr + 5 + idx * DOWN);
+                BYTE low = *((PBYTE)addr + 4 + idx * DOWN);
+                syscall = (high << 8) | low - idx;
+
+                return syscall;
+            }
+            if (*((PBYTE)addr + idx * UP) == 0x4c
+                && *((PBYTE)addr + 1 + idx * UP) == 0x8b
+                && *((PBYTE)addr + 2 + idx * UP) == 0xd1
+                && *((PBYTE)addr + 3 + idx * UP) == 0xb8
+                && *((PBYTE)addr + 6 + idx * UP) == 0x00
+                && *((PBYTE)addr + 7 + idx * UP) == 0x00) {
+                BYTE high = *((PBYTE)addr + 5 + idx * UP);
+                BYTE low = *((PBYTE)addr + 4 + idx * UP);
+                syscall = (high << 8) | low + idx;
+
+                return syscall;
+
+            }
+
+        }
+
+    }
+
+    if (*((PBYTE)addr + 10) == 0xe9) {
+
+        for (WORD idx = 1; idx <= 500; idx++) {
+            if (*((PBYTE)addr + idx * DOWN) == 0x4c
+                && *((PBYTE)addr + 1 + idx * DOWN) == 0x8b
+                && *((PBYTE)addr + 2 + idx * DOWN) == 0xd1
+                && *((PBYTE)addr + 3 + idx * DOWN) == 0xb8
+                && *((PBYTE)addr + 6 + idx * DOWN) == 0x00
+                && *((PBYTE)addr + 7 + idx * DOWN) == 0x00) {
+                BYTE high = *((PBYTE)addr + 5 + idx * DOWN);
+                BYTE low = *((PBYTE)addr + 4 + idx * DOWN);
+                syscall = (high << 8) | low - idx;
+
+                return syscall;
+            }
+            if (*((PBYTE)addr + idx * UP) == 0x4c
+                && *((PBYTE)addr + 1 + idx * UP) == 0x8b
+                && *((PBYTE)addr + 2 + idx * UP) == 0xd1
+                && *((PBYTE)addr + 3 + idx * UP) == 0xb8
+                && *((PBYTE)addr + 6 + idx * UP) == 0x00
+                && *((PBYTE)addr + 7 + idx * UP) == 0x00) {
+                BYTE high = *((PBYTE)addr + 5 + idx * UP);
+                BYTE low = *((PBYTE)addr + 4 + idx * UP);
+                syscall = (high << 8) | low + idx;
+
+                return syscall;
+
+            }
+
+        }
+
+    }
+
+    if (*((PBYTE)addr + 12) == 0xe9) {
+
+        for (WORD idx = 1; idx <= 500; idx++) {
+            if (*((PBYTE)addr + idx * DOWN) == 0x4c
+                && *((PBYTE)addr + 1 + idx * DOWN) == 0x8b
+                && *((PBYTE)addr + 2 + idx * DOWN) == 0xd1
+                && *((PBYTE)addr + 3 + idx * DOWN) == 0xb8
+                && *((PBYTE)addr + 6 + idx * DOWN) == 0x00
+                && *((PBYTE)addr + 7 + idx * DOWN) == 0x00) {
+                BYTE high = *((PBYTE)addr + 5 + idx * DOWN);
+                BYTE low = *((PBYTE)addr + 4 + idx * DOWN);
+                syscall = (high << 8) | low - idx;
+
+                return syscall;
+            }
+            if (*((PBYTE)addr + idx * UP) == 0x4c
+                && *((PBYTE)addr + 1 + idx * UP) == 0x8b
+                && *((PBYTE)addr + 2 + idx * UP) == 0xd1
+                && *((PBYTE)addr + 3 + idx * UP) == 0xb8
+                && *((PBYTE)addr + 6 + idx * UP) == 0x00
+                && *((PBYTE)addr + 7 + idx * UP) == 0x00) {
+                BYTE high = *((PBYTE)addr + 5 + idx * UP);
+                BYTE low = *((PBYTE)addr + 4 + idx * UP);
+                syscall = (high << 8) | low + idx;
+
+                return syscall;
+
+            }
+
+        }
+
+    }
 }
 
 
@@ -237,6 +369,114 @@ INT_PTR Unh00ksyscallInstr(LPVOID addr) {
                 && *((PBYTE)addr + 7 + idx * UP) == 0x00) {
                 
                 return (INT_PTR)addr + 0x12; 
+
+            }
+
+        }
+
+    }
+
+    if (*((PBYTE)addr + 3) == 0xe9) {
+
+        for (WORD idx = 1; idx <= 500; idx++) {
+            if (*((PBYTE)addr + idx * DOWN) == 0x4c
+                && *((PBYTE)addr + 1 + idx * DOWN) == 0x8b
+                && *((PBYTE)addr + 2 + idx * DOWN) == 0xd1
+                && *((PBYTE)addr + 3 + idx * DOWN) == 0xb8
+                && *((PBYTE)addr + 6 + idx * DOWN) == 0x00
+                && *((PBYTE)addr + 7 + idx * DOWN) == 0x00) {
+
+                return (INT_PTR)addr + 0x12;
+            }
+            if (*((PBYTE)addr + idx * UP) == 0x4c
+                && *((PBYTE)addr + 1 + idx * UP) == 0x8b
+                && *((PBYTE)addr + 2 + idx * UP) == 0xd1
+                && *((PBYTE)addr + 3 + idx * UP) == 0xb8
+                && *((PBYTE)addr + 6 + idx * UP) == 0x00
+                && *((PBYTE)addr + 7 + idx * UP) == 0x00) {
+
+                return (INT_PTR)addr + 0x12;
+
+            }
+
+        }
+
+    }
+
+    if (*((PBYTE)addr + 8) == 0xe9) {
+
+        for (WORD idx = 1; idx <= 500; idx++) {
+            if (*((PBYTE)addr + idx * DOWN) == 0x4c
+                && *((PBYTE)addr + 1 + idx * DOWN) == 0x8b
+                && *((PBYTE)addr + 2 + idx * DOWN) == 0xd1
+                && *((PBYTE)addr + 3 + idx * DOWN) == 0xb8
+                && *((PBYTE)addr + 6 + idx * DOWN) == 0x00
+                && *((PBYTE)addr + 7 + idx * DOWN) == 0x00) {
+
+                return (INT_PTR)addr + 0x12;
+            }
+            if (*((PBYTE)addr + idx * UP) == 0x4c
+                && *((PBYTE)addr + 1 + idx * UP) == 0x8b
+                && *((PBYTE)addr + 2 + idx * UP) == 0xd1
+                && *((PBYTE)addr + 3 + idx * UP) == 0xb8
+                && *((PBYTE)addr + 6 + idx * UP) == 0x00
+                && *((PBYTE)addr + 7 + idx * UP) == 0x00) {
+
+                return (INT_PTR)addr + 0x12;
+
+            }
+
+        }
+
+    }
+
+    if (*((PBYTE)addr + 10) == 0xe9) {
+
+        for (WORD idx = 1; idx <= 500; idx++) {
+            if (*((PBYTE)addr + idx * DOWN) == 0x4c
+                && *((PBYTE)addr + 1 + idx * DOWN) == 0x8b
+                && *((PBYTE)addr + 2 + idx * DOWN) == 0xd1
+                && *((PBYTE)addr + 3 + idx * DOWN) == 0xb8
+                && *((PBYTE)addr + 6 + idx * DOWN) == 0x00
+                && *((PBYTE)addr + 7 + idx * DOWN) == 0x00) {
+
+                return (INT_PTR)addr + 0x12;
+            }
+            if (*((PBYTE)addr + idx * UP) == 0x4c
+                && *((PBYTE)addr + 1 + idx * UP) == 0x8b
+                && *((PBYTE)addr + 2 + idx * UP) == 0xd1
+                && *((PBYTE)addr + 3 + idx * UP) == 0xb8
+                && *((PBYTE)addr + 6 + idx * UP) == 0x00
+                && *((PBYTE)addr + 7 + idx * UP) == 0x00) {
+
+                return (INT_PTR)addr + 0x12;
+
+            }
+
+        }
+
+    }   
+
+    if (*((PBYTE)addr + 12) == 0xe9) {
+
+        for (WORD idx = 1; idx <= 500; idx++) {
+            if (*((PBYTE)addr + idx * DOWN) == 0x4c
+                && *((PBYTE)addr + 1 + idx * DOWN) == 0x8b
+                && *((PBYTE)addr + 2 + idx * DOWN) == 0xd1
+                && *((PBYTE)addr + 3 + idx * DOWN) == 0xb8
+                && *((PBYTE)addr + 6 + idx * DOWN) == 0x00
+                && *((PBYTE)addr + 7 + idx * DOWN) == 0x00) {
+
+                return (INT_PTR)addr + 0x12;
+            }
+            if (*((PBYTE)addr + idx * UP) == 0x4c
+                && *((PBYTE)addr + 1 + idx * UP) == 0x8b
+                && *((PBYTE)addr + 2 + idx * UP) == 0xd1
+                && *((PBYTE)addr + 3 + idx * UP) == 0xb8
+                && *((PBYTE)addr + 6 + idx * UP) == 0x00
+                && *((PBYTE)addr + 7 + idx * UP) == 0x00) {
+
+                return (INT_PTR)addr + 0x12;
 
             }
 
